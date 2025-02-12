b8d6f509f6c9f65daa6c7d68fb1a0e6737ed1454
Maulik Garg <maulikgarg678@gmail.com>
2025-02-12 21:26:11 +0530

    add getChar and didInpWork functions

--- getInput.cpp
+++ getInput.cpp
@@ -6,20 +6,46 @@
 
 namespace getValidInput {
 void ignoreLine() {
+  // ignores any extra character which may be in the input
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+bool didInpWork() {
+  // if cin failed, return it to safe state and return false
+  if (!std::cin) {
+    std::cin.clear();
+    ignoreLine();
+    std::cout << "That was invalid input. Please try again.\n>";
+    return false;
+  }
+  return true;
 }
 
 std::string getString() {
   std::string str{};
   while (true) {
+    // gets a full whitespaced string
     std::getline(std::cin >> std::ws, str);
-    if (!std::cin) {
-      std::cin.clear();
-      ignoreLine();
-      std::cout << "That was invalid input. Please try again.\n>";
-    } else {
-      return str;
+    // if input worked, return it safely
+    if (didInpWork()) return str;
+  }
+}
+
+char getChar(const std::string& allowed = {}) {
+  char ch;
+  while (true) {
+    std::cin >> ch;
+    if (!didInpWork()) continue;
+    // if the allowed vector was not provided, return the current character
+    if (allowed.empty()) return ch;
+    // compare the entered character against the allowed values
+    for (auto& i : allowed) {
+      // if a character matches, return the input
+      if (i == ch) {
+        return ch;
+      }
     }
+    std::cout << "That was invalid, please try again: ";
   }
 }
 }  // namespace getValidInput
--- getInput.h
+++ getInput.h
@@ -1,9 +1,14 @@
 #ifndef GET_INPUT_H
 #define GET_INPUT_H
 #include <string>
+#include <vector>
 
 namespace getValidInput{
+   //gets a valid string and returns it
    std::string getString();
+   //gets a valid integer integer and allows it
    int getValidInt();
+   //gets a valid character, can optionally except a pool to pick from
+   char getChar(const std::string& allowed = {});
 }
 #endif
