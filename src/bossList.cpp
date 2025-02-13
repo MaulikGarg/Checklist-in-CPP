2bc904b64337fcb0cd15aa67ae0c6f47c7dac2b7
Maulik Garg <maulikgarg678@gmail.com>
2025-02-13 10:50:46 +0530

    update readability

--- src/bossList.cpp
+++ src/bossList.cpp
@@ -33,7 +33,7 @@ void BossList::addList() {
       break;
     }
   }
-  std::cout << "List " << name << " has been created.\n";
+  std::cout << "\nList " << name << " has been created.\n";
   std::cout << "Would you like to add elements to your list (y/n): ";
   // stores the response of the user
   char response = getValidInput::getChar(std::string{"ynYN"});
@@ -45,35 +45,36 @@ void BossList::addList() {
 
 // removes a specific list from the bosslist
 void BossList::removeList() {
-  std::cout << "Please enter the name of the list you would like to "
+  std::cout << "\nPlease enter the name of the list you would like to "
                "remove(type 0 to cancel operation): ";
   std::string name{getValidInput::getString()};
   // check if the user wishes to cancel operation
   if (name == "0") {
-    std::cout << "Operation cancelled.\n";
+    std::cout << "\nOperation cancelled.\n";
     return;
   }
   // lookup the name in the list of lists
   auto removable_it{m_mainlist.find(name)};
   // if the list does not exist, print a message and return
   if (removable_it == m_mainlist.end()) {
-    std::cout << "That list does not exist.\n";
+    std::cout << "\nThat list does not exist.\n";
     return;
   }
   // remove the element from the map
   m_mainlist.erase(removable_it);
-  std::cout << "List " << name << " has been removed.\n";
+  std::cout << "\nList " << name << " has been removed.\n";
 }
 
 // shows all the lists currently saved to the disk
 void BossList::showLists() {
   //if list count is zero
   if(m_mainlist.size() == 0){
-    std::cout << "There are no lists to show!\n";
+    std::cout << "\nThere are no lists to show!\n";
     return;
   }
   // count to keep track of the number of lists
   int count{};
+  std::cout << "\n===Lists===\n";
   for (const auto& i : m_mainlist) {
     std::cout << ++count << ". " << i.first << '\n';
   }
@@ -89,7 +90,7 @@ void BossList::showMenu() {
       std::cout << i << ". " << str_options[i] << '\n';
     }
     std::cout
-        << "Please enter the ID of the operation you would like to perform: ";
+        << "\nPlease enter the ID of the operation you would like to perform: ";
 
     // get a response and act on it.
     switch (static_cast<options>(getValidInput::getInt(options::exit-1,options::max)))  {
