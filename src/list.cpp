5b8bc71e83af92ae4c3335fc62d8ae68b7d414ec
Maulik Garg <maulikgarg678@gmail.com>
2025-02-13 11:24:50 +0530

    add addElement

--- src/list.cpp
+++ src/list.cpp
@@ -3,6 +3,23 @@
 #include <iostream>
 
 #include "getInput.h"
-bool List::addElement() {}
+void List::addElement() {
+  while (true) {
+    std::cout << "Enter the element(Type 0 to eliminate operation): ";
+    // get the element to the input
+    std::string element{getValidInput::getString()};
+    //check if the user wishes to terminte
+    if(element == "0") return;
+    // attempt to insert it into the set of elements, store the resulting pair
+    // in 'result'
+    auto result = m_elements.insert(element);
+    // if result has failed, then print message and continue the loop
+    if (!result.second) {
+      std::cout << element << " already exists in this list!\n";
+      continue;
+    }
+    std::cout << element << " has been added to the list.\n";
+  }
+}
 
 bool List::removeElement() {}
--- src/list.h
+++ src/list.h
@@ -1,20 +1,19 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <string>
 #include <set>
-class List{
-   private:
-      std::set<std::string> m_elements {};
-   public: 
-      //adds an element to the list, returns 1 if add successful and 0 if failure
-      bool addElement();
-      //removes an element to the list, returns 1 if add successful and 0 if failure
-      bool removeElement();
-      //operator < to determine if a List should come after or before
-      //mainly needed to sort the boss list, and for the set container
-      bool operator< (const List& rhs) const;
+#include <string>
+class List {
+ private:
+  std::set<std::string> m_elements{};
+  // shows all the elements in the list
+  void printList();
 
+ public:
+  // adds element(s) to the list
+  void addElement();
+  // removes an element to the list
+  void removeElement();
 };
 
 #endif
