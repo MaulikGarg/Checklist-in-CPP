99f0ab35cd1ce4f1c10c3be62ae4933ead718e5e
Maulik Garg <maulikgarg678@gmail.com>
2025-02-14 20:45:15 +0530

    fix #1, add sync flags

--- src/list.h
+++ src/list.h
@@ -26,6 +26,8 @@ static_assert(max == std::size(str_options));
 
 class List {
  private:
+  // bool to indicate whether changes have been made to the list.
+  bool m_saveSync{true};
   std::set<std::string> m_elements{};
 
  public:
@@ -33,12 +35,14 @@ class List {
   void printList();
   // adds element(s) to the list
   void addElement();
-  // adds a given element to the list, returns false if addition failed
+  // adds a given element to the list, returns false if addition failed, only
+  // intended for automatic use, not by user
   bool addElement(const std::string&);
   // removes an element to the list
   void removeElement();
-  // shows a basic menu interface
-  void showMenu();
+  // shows a basic menu interface, returns the save status of the list save, 1
+  // if synced with save, 0 if not
+  bool showMenu();
   // returns the elemental list as a constant reference, used for file writing
   const std::set<std::string>& getList() const { return m_elements; }
 };
