bae0be1dad89d0374c74faffdc2eff9144b0197d
Maulik Garg <maulikgarg678@gmail.com>
2025-02-16 15:10:02 +0530

    add testing foundation

--- tests/getInputTest.cpp
+++ tests/getInputTest.cpp
@@ -1,1 +1,11 @@
 //tests all the commands in getInput module
+
+#include <catch2/catch_all.hpp>
+#include <iostream>
+#include "../src/getInput.h"
+
+TEST_CASE("Test getInput function") {
+  std::stringstream input("kk\n5\n");
+  std::cin.rdbuf(input.rdbuf());
+  REQUIRE(getValidInput::getInt() == 5);
+}
