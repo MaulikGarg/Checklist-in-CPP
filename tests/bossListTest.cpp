e74ea0259e4627406b5892a7acf16e6178d5e15d
Maulik Garg <maulikgarg678@gmail.com>
2025-02-27 22:05:30 +0530

    add wrapper class for cin cout

--- tests/bossListTest.cpp
+++ tests/bossListTest.cpp
@@ -11,40 +11,57 @@ using namespace std::string_literals;
 
 using void_lambda = std::function<void()>;
 
+class bufferCapture {
+ private:
+ // hold the initial buffer pointers
+  std::streambuf* inputBackup;
+  std::streambuf* outputBackup;
+  // custom input for cin replacement
+  std::stringstream inputStream;
+  // stores cout's output
+  std::stringstream outputStream;
+
+ public:
+  bufferCapture(const std::string& inp = "") {
+    // make backups and set cin cout to custom buffer
+    inputBackup = std::cin.rdbuf();
+    outputBackup = std::cout.rdbuf();
+    std::cin.rdbuf(inputStream.rdbuf());
+    std::cout.rdbuf(outputStream.rdbuf());
+    setInput(inp);
+  }
+
+  ~bufferCapture() {
+    // use backup and return cin cout to normal
+    std::cin.rdbuf(inputBackup);
+    std::cout.rdbuf(outputBackup);
+  }
+
+  // set custom input
+  void setInput(const std::string& inp) { inputStream.str(inp); }
+  // get the output
+  std::string getOutput() { return outputStream.str(); }
+};
+
+/* ---------- constructor tests ------------------- */
+
 TEST_CASE("Empty list name test") {
-  // redirect cin and cout for testing.
 
-  //  make an output stream
-  std::stringstream output{};
-  // save current buffer pointer so we can redirect cout back to it later
-  auto coutBuffer = std::cout.rdbuf();
-  // all coutput goes into the stream
-  std::cout.rdbuf(output.rdbuf());
+  bufferCapture b1{"0"};
 
-  // make an input stream
-  std::stringstream input{"0"};
-  // save the current input for later redirection
-  auto cinBuffer = std::cin.rdbuf();
-  // set cin's buffer to out string
-  std::cin.rdbuf(input.rdbuf());
-
-  BossList b1{true};  // set testing flag to true
+  BossList testlist{true};  // set testing flag to true
 
   // initial constructor greeting
   std::string expected_output{
       "Please enter the name of your collection file(type 0 to create "
       "new): "};
 
-  // try entering value 0    
-  input << "0";
-  
+
   // new list should be made when 0 is input
-  expected_output+="New empty list made successfully!\n"s;
+  expected_output += "New empty list made successfully!\n"s;
 
-  REQUIRE(expected_output == output.str());
-
-  // return cin and cout
-  std::cout.rdbuf(coutBuffer);
-  std::cin.rdbuf(cinBuffer);
+  REQUIRE(expected_output == b1.getOutput());
 
 }
+
+TEST_CASE("Non alphanumeric input file name") {}
